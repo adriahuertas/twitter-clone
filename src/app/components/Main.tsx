f7380d138f15732b70b4005780f761d53bc6423f
Adria Huertas <adriahc@gmail.com>
2023-10-14 18:37:50 +0200

    Styled search twitter input

--- src/app/components/Main.tsx
+++ src/app/components/Main.tsx
@@ -4,7 +4,7 @@ import { IoShareOutline, IoStatsChart } from 'react-icons/io5'
 
 const Main = () => {
   return (
-    <main className="flex w-[56%] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
+    <main className="flex w-[600px] h-full min-h-screen flex-col border-l-[0.5px] border-r-[0.5px] border-gray-600">
       <h1 className="text-xl font-bold backdrop-blur p-6 bg-black/10 sticky top-0">Home</h1>
       <div className='border-t-[0.5px] border-b-[0.5px] px-4 flex items-stretch py-4 space-x-2 border-gray-600  relative'>
         <div className="w-10 h-10 bg-slate-400 rounded-full flex-none"></div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,17 +5,21 @@ import Main from './components/Main'
 const Home = () => {
   return (
     <div className="w-full h-full flex justify-center items-center relative bg-black">
-      <div className="max-w-[1100px] w-full h-full flex relative" >
+      <div className="max-w-[1150px] w-full h-full flex relative" >
         {/* Left sidebar / header */}
         <LeftSidebar />
         <Main />
-        <section className=" sticky top-0 w-[22%] flex flex-col items-stretch h-screen px-6">
+        <section className=" sticky top-2 w-[275px] mt-2 flex flex-col items-stretch h-screen px-4">
           <div>
-            <div className="relative w-full h-full">
-              <label htmlFor="searchBox" className="absolute top-0 left-0 h-full flex items-center justify-center">
-                <BsSearch className="w-5 h-5 text-gray-500" />
+            <div className="relative w-full h-full group">
+              <input
+                id="searchBox"
+                type="text"
+                placeholder="Search Twitter"
+                className="outline-none peer focus:border-primary focus:border bg-neutral-900/90 w-full h-full rounded-xl py-3 pl-12" />
+              <label htmlFor="searchBox" className="absolute top-0 left-0 h-full flex items-center justify-center p-4 peer-focus:text-primary text-gray-500">
+                <BsSearch className="w-5 h-5" />
               </label>
-              <input id="searchBox" type="text" placeholder="Search Twitter" className="outline-none bg-transparent w-full h-full rounded-xl py-4 px-8" />
             </div>
           </div>
           <div></div>
